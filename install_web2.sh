180bf63fcd0660070f8169208c281a031062ae29
arielfrozza <arielfrozza@gmail.com>
2021-12-24 10:10:11 -0300

    Update install_web2.sh

--- install_web2.sh
+++ install_web2.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-sudo yum install -y httpd
-sudo systemctl enable httpd
-sudo systemctl start httpd  
-sudo systemctl stop firewalld
-sudo systemctl disable firewalld
-sudo chmod -R 777 /var/www/html 
+#sudo yum install -y httpd
+#sudo systemctl enable httpd
+#sudo systemctl start httpd  
+#sudo systemctl stop firewalld
+#sudo systemctl disable firewalld
+#sudo chmod -R 777 /var/www/html 
 sudo mkdir /var/www/html/app2
 sudo echo "Welcome to stacksimplify - WebVM App2 - VM Hostname: $(hostname)" > /var/www/html/app2/hostname.html
